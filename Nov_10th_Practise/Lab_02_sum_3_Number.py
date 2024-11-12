b5f9d0cdb7315e35cd28ae506efa68bd2d23e302
Rompalvi <rompally.vilasreddy@gmail.com>
2024-11-12 08:26:36 +0530

    code pushed nov 11th

---
+++ Nov_10th_Practise/Lab_02_sum_3_Number.py
@@ -0,0 +1,16 @@
+Num1=int(input("enter num1"))
+Num2=int(input("enter num2"))
+Num3=int(input("enter num3"))
+print(type("Num1"))
+print(type("Num2"))
+print(type("Num3"))
+
+Sum=Num1+Num2+Num3
+Sub=Num1-Num2-Num3
+Multi=Num1*Num2*Num3
+Div=Num1%Num2%Num3
+
+print("Sum is :", Sum)
+print("Sub is :", Sub)
+print("Multi is : ", Multi)
+print("Div is :", Div)
